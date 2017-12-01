42ad0de7f567b285b30cd85a72b0326e6254eb6c
Eddie Peters <edward.paul.peters@gmail.com>
2017-12-01 12:03:50 -0500

    Simplest possible wsgi app in wes.

--- dfswes/dfswes/wsgi.py
+++ dfswes/dfswes/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dfsdjango project.
+WSGI config for dfswes project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
 
-from django.core.wsgi import get_wsgi_application
+def wsgi_app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return [b'Hello world!\n\t- Love, Wes']
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dfsdjango.settings")
-
-application = get_wsgi_application()
+application = wsgi_app

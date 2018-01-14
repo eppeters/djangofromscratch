9e49025e25e61bc18b51151ba47eee5b16d8a390
Eddie Peters <edward.paul.peters@gmail.com>
2018-01-14 13:17:34 -0500

    Trace dfswes and dfsdjango.

--- dfsdjango/dfsdjango/wsgi.py
+++ dfsdjango/dfsdjango/wsgi.py
@@ -13,4 +13,40 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dfsdjango.settings")
 
-application = get_wsgi_application()
+
+def trace_wsgi(environ, start_response):
+    import sys
+    import trace
+
+    print(sys.prefix, sys.exec_prefix)
+
+    # create a Trace object, telling it what to ignore, and whether to
+    # do tracing or line-counting or both.
+    tracer = trace.Trace(
+        trace=1,
+        count=0)
+
+    from io import StringIO
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    # run the new command using the given tracer
+    wsgi_return = tracer.runfunc(get_wsgi_application(), environ, start_response)
+    sys.stdout = old_stdout
+    out = mystdout.getvalue()
+    outfile = open('trace.out', 'a+')
+    outfile.write('=======================EDDIETRACE=========================\n')
+    outfile.write(out)
+    outfile.close()
+
+    # make a report, placing output in the current directory
+    r = tracer.results()
+    r.write_results(show_missing=True, coverdir=".")
+
+    return wsgi_return
+
+
+def wsgi_app(environ, start_response):
+    return trace_wsgi(environ, start_response)
+
+
+application = wsgi_app
--- dfswes/dfswes/wsgi.py
+++ dfswes/dfswes/wsgi.py
@@ -8,10 +8,34 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 
+def trace_wsgi(environ, start_response):
+    import sys
+    import trace
+
+    print(sys.prefix, sys.exec_prefix)
+
+    # create a Trace object, telling it what to ignore, and whether to
+    # do tracing or line-counting or both.
+    tracer = trace.Trace(
+        ignoredirs=[sys.prefix, sys.exec_prefix],
+        trace=1,
+        count=1,
+        outfile='out.trace')
+
+    # run the new command using the given tracer
+    wsgi_return = tracer.runfunc(wsgi_app, environ, start_response)
+
+    # make a report, placing output in the current directory
+    r = tracer.results()
+    r.write_results(show_missing=True, coverdir=".")
+
+    return wsgi_return
+
+
 def wsgi_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return [b'Hello world!\n\t- Love, Wes']
 
-application = wsgi_app
+application = trace_wsgi
